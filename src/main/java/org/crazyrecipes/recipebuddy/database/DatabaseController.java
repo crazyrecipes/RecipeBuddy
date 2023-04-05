1e85c3ef5a2d8b4c20df3e60a4c709bba4afb933
CrazyHuman1 <94556102+CrazyHuman1@users.noreply.github.com>
2023-04-04 23:20:26 -0400

    Start of implementation of Database

---
+++ src/main/java/org/crazyrecipes/recipebuddy/database/DatabaseController.java
@@ -0,0 +1,10 @@
+package org.crazyrecipes.recipebuddy.database;
+
+import org.crazyrecipes.recipebuddy.database.Database;
+import org.crazyrecipes.recipebuddy.recipe.*;
+import org.crazyrecipes.recipebuddy.ingredient.*;
+import org.crazyrecipes.recipebuddy.utensil.*;
+import org.crazyrecipes.recipebuddy.allergens.*;
+
+public class DatabaseController {
+}
