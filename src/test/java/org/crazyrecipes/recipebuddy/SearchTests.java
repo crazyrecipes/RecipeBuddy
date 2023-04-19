f2cf1696d7a6946bb13fd62a5f46d11a62f2006e
Juno <jvmeifert@gmail.com>
2023-04-19 19:52:00 -0400

    Add Search tests

---
+++ src/test/java/org/crazyrecipes/recipebuddy/SearchTests.java
@@ -0,0 +1,36 @@
+package org.crazyrecipes.recipebuddy;
+
+import org.crazyrecipes.recipebuddy.recipe.Recipe;
+import org.crazyrecipes.recipebuddy.search.Search;
+import org.crazyrecipes.recipebuddy.util.Log;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Vector;
+
+/**
+ * Tests the Search data structure.
+ */
+@SpringBootTest
+class SearchTests {
+
+	/**
+	 * Tests instantiation of Search.
+	 */
+	@Test
+	public void testSearch() {
+		Log log = new Log("SearchTests");
+		log.print("===== Beginning Search test. =====");
+
+		log.print("Creating test Search...");
+		Search test_search = new Search("burger", "SOME", "SOME", "BLOCK");
+
+		log.print("Checking integrity...");
+		assert test_search.getQuery().equals("burger");
+		assert test_search.getIngredients().equals("SOME");
+		assert test_search.getUtensils().equals("SOME");
+		assert test_search.getAllergens().equals("BLOCK");
+
+		log.print("===== Recipe test passed. =====");
+	}
+}
